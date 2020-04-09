e6fe1017813da5f834f582cbe8a18dbed9613b0f
Joachim Hejlesen <joachim-dk@hotmail.com>
2020-04-09 22:53:29 +0200

    Turns in items

--- src/farmer.lua
+++ src/farmer.lua
@@ -2,6 +2,9 @@ print("starting")
 local lastslot = 16
 local error = false
 local turned = false
+local harvesting = true
+local turningItemsIn = false
+local storageFull = false
 turtle.select(1)
 
 function harvest()
@@ -30,7 +33,7 @@ function moveFarming()
         if data.name == "minecraft:cobblestone" then
             turtle.turnLeft()
         end
-        if data.name == "minecraft:spruce_planks" then
+        if data.name == "minecraft:smooth_stone" then
             return false
         end
     end
@@ -48,15 +51,52 @@ function moveFarming()
     end
 end
 
-function getToFarm()
-
-end
-
 function returnItems()
-
+    turtle.forward()
+    turtle.forward()
+    turtle.turnRight()
+    for i=1, 6 do
+        turtle.forward()
+    end
+    for i=2, 16 do
+        if not storageFull then
+            local success = turtle.dropDown(i)
+            if not success then
+                storageFull = true
+            end
+        end
+    end
+    turtle.turnRight()
+    turtle.turnRight()
+    for i=1, 6 do
+        turtle.forward()
+    end
+    turtle.turnLeft()
+    turtle.forward()
+    turtle.forward()
 end
-turtle.forward()
-for i=1, 20 do
-    moveFarming()
-    harvest()
+
+--turtle.forward()
+harvesting = false
+turningItemsIn = true
+while true do
+    if harvesting then
+        moveFarming()
+        harvest()
+    else if turningItemsIn then
+        
+        returnItems()
+        if storageFull then
+            print("Storage full")
+            print("Turning off")
+            os.shutdown()
+            return
+        end
+        print("Going to sleep")
+        print("Harvesting in 200 seconds")
+        os.sleep(200)
+        turtle.forward()
+        turningItemsIn = false
+        harvesting = true
+    end
 end
