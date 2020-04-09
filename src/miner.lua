66704a881fb92f71c4f05a56054e25b5f424be35
Joachim Hejlesen <joachim-dk@hotmail.com>
2020-04-09 20:21:43 +0200

    Made robot return after work

--- src/miner.lua
+++ src/miner.lua
@@ -1,18 +1,49 @@
 print("starting")
+local lastslot = 16
+local startpos = 0
+local notReturning = true
+function checkselectedslot()
+    local slot = turtle.getSelectedSlot()
+    if turtle.getItemCount() > 0 then
+        return true
+    else
+        if slot == lastslot then
+            return false
+        else
+            slot++
+            turtle.select(slot)
+            return checkselectedslot()
+        end
+
+    end
+end
+
 
 if not turtle.detect() then
+    turtle.forward()
     turtle.forward()
 end
 
 turtle.turnRight()
 turtle.turnRight()
 
-while not turtle.detectDown() do
-    turtle.down()
-    if turtle.compare() then
+while not turtle.detectDown() and notReturning do
+    if checkselectedslot() then
+        turtle.down()
         turtle.place()
+        startpos--
+    else
+        notReturning = false
     end
 end
 
+for i=startpos, 0 do
+    turtle.up()
+end
+
+turtle.forward()
+turtle.forward()
+turtle.turnRight()
+turtle.turnRight()
 
 print("Staircase Built")
